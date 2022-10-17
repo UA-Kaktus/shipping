ae1ec4dca7130125f013df1b88e61d05ea017b08
Yurii <legostaevyura999@gmail.com>
2022-10-17 23:52:41 +0300

    last commit

--- src/js/script.js
+++ src/js/script.js
@@ -29,4 +29,63 @@ document.addEventListener('DOMContentLoaded', () => {
             mobileMenu.style.left = '-100%';
         });
     });
+
+    const slyder = document.querySelector('.review__slyder');
+    const slyderItem = document.querySelectorAll('.review__item');
+    const slyderLeftArrow = document.querySelector('.review__arrow-left');
+    const slyderRightArrow = document.querySelector('.review__arrow-right');
+    let slyderCounter = 1;
+
+    slyderLeftArrow.addEventListener('click', () => {
+        if(slyderCounter == 0) {
+            slyderItem[0].classList.add('review__item_active-left');
+            setTimeout(() => {
+                slyderItem[0].classList.remove('review__item_active-left');
+            }, 400);
+            // animation below also works, just using native JS
+            // slyderItem[0].animate([{transform: 'translateX(0) scale(150%)'}, {transform: 'translateX(15px) scale(150%)'},{transform: 'translateX(0) scale(150%)'}], {duration: 500, iterations: 1});
+            return;
+        }
+        let slyderItemWidth = +window.getComputedStyle(slyderItem[0]).getPropertyValue('width').slice(0,-2);
+        slyderCounter -= 1;
+        slyderItem.forEach(el => {
+            el.classList.remove('review__item_active');
+        });
+        slyderItem[slyderCounter].classList.add('review__item_active');
+        slyder.style.left = `${-slyderCounter * (slyderItemWidth + 30)}px`;
+    });
+
+    slyderRightArrow.addEventListener('click', () => {
+        if(slyderCounter == slyderItem.length - 1) {
+            slyderItem[slyderCounter].classList.add('review__item_active-right');
+            setTimeout(() => {
+                slyderItem[slyderCounter].classList.remove('review__item_active-right');
+            }, 400);
+            return;
+        }
+        //better use last item in "slyderItem" array, cause of changing width, but in this case doesn't matter what to use
+        let slyderItemWidth = +window.getComputedStyle(slyderItem[0]).getPropertyValue('width').slice(0,-2);
+        slyderCounter += 1;
+        slyderItem.forEach(el => {
+            el.classList.remove('review__item_active');
+        });
+        slyderItem[slyderCounter].classList.add('review__item_active');
+        slyder.style.left = `${-slyderCounter * (slyderItemWidth + 30)}px`;
+    });
+
+    const modalWindow = document.querySelector('.modal');
+    const modalWindowClose = document.querySelector('.modal__close');
+    const modalWindowButtons = document.querySelectorAll('[data-modal = "modal-window"]');
+
+    modalWindowButtons.forEach(el => {
+        el.addEventListener('click', () => {
+            modalWindow.style.display = 'block';
+        });
+    });
+
+    modalWindow.addEventListener('click', (event) => {
+        if (event.target == modalWindow || event.target == modalWindowClose) {
+            modalWindow.style.display = 'none';
+        }
+    });
 });
